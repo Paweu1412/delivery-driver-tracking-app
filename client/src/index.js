27f01a21db5b481c9717f6cd7c5c527c385ad5d8
Paweł Nosalski <fordcrown2009@gmail.com>
2024-03-16 20:54:47 +0100

    new changes

--- client/src/index.js
+++ client/src/index.js
@@ -1,8 +1,10 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
 import { BrowserRouter, Routes, Route } from 'react-router-dom';
+import { MantineProvider } from '@mantine/core';
 
 import './index.css';
+import '@mantine/core/styles.css';
 
 import Login from './views/Login/Login';
 
@@ -10,10 +12,12 @@ const root = ReactDOM.createRoot(document.getElementById('root'));
 
 root.render(
   <React.StrictMode>
-    <BrowserRouter>
-      <Routes>
-        <Route path="*" element={<Login />} />
-      </Routes>
-    </BrowserRouter>
+    <MantineProvider>
+      <BrowserRouter>
+        <Routes>
+          <Route path="*" element={<Login />} />
+        </Routes>
+      </BrowserRouter>
+    </MantineProvider>
   </React.StrictMode>
 );
--- client/src/views/Login/Login.js
+++ client/src/views/Login/Login.js
@@ -1,7 +1,28 @@
+import { IconUser, IconKey } from '@tabler/icons-react';
+import { Input, Button } from '@mantine/core';
+
+import Logo from '../../assets/logo.jpg';
+
 const Login = () => {
   return (
-    <div className="Login flex w-full justify-center">
-      dd
+    <div className="Login flex w-screen h-screen flex-col items-center justify-center">
+      <img src={Logo} alt="Logo" className="size-[180px] -mt-10" />
+
+      <div className="form mt-5">
+        <h1 className="text-2xl font-bold">Zaloguj się do systemu</h1>
+
+        <div className="credentials mt-6">
+          <div className="login mb-4">
+            <Input placeholder="Identyfikator" leftSection={<IconUser size={16} />} />
+          </div>
+
+          <div className="password">
+            <Input placeholder="Hasło" type="password" leftSection={<IconKey size={16} />} />
+          </div>
+
+          <Button color="black" className="mt-6" fullWidth>Zaloguj się</Button>
+        </div>
+      </div>
     </div>
   );
 }
